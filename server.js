0919a269dff21b6f3071ce785d5b1f8fa9a32e49
Steven Salto <ssalto2702@gmail.com>
2023-04-30 23:57:08 -0400

    changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const { PORT } = require('./config');
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
