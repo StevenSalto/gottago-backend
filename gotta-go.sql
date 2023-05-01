2adc0bcdafec32bab7468af9c0629995bed74115
Steven Salto <ssalto2702@gmail.com>
2023-05-01 00:31:10 -0400

    fsf

--- gotta-go.sql
+++
@@ -1,9 +0,0 @@
-\echo 'DELETING and creating database gotta-go'
-\prompt 'Any key for "yes", Ctrl+C for "no"' answer
-
-DROP DATABASE gotta_go;
-CREATE DATABASE gotta_go;
-
-\connect gotta_go;
-
-\i gotta-go-schema.sql
